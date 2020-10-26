de6f6461a22e54f874a84bfad1bd61e83a4efdb7
downlodpark12 <downlodpark12@gmail.com>
2020-10-26 18:18:16 +0900

    1026 오늘 방송들어

--- public/js/index.js
+++ public/js/index.js
@@ -1,57 +1,57 @@
-/***** 전역변수 선언 ***********************/
-var subNow = 0;		//.navi.full 에서의 슬라이드 변수 
-var subLast = 3;	//.navi.full 에서의 슬라이드 변수 
+/** 전역변수선언 ************************************/
+var subNow = 0;		// .navi.FULL 에서의 슬라이드변수
+var subLast = 3;	// .navi.FULL 에서의 슬라이드변수
 
 
-
-/***** 사용자 지정 함수 ********************/
+/** 사용자 지정 함수 ********************************/
 
 // .navi.FULL 에서의 슬라이드
 function subAni() {
-	$(".sub-slide .wrap").stop().animate({"left": -100 * subNow +"%"}, 500, function(){
-		if(subNow == subLast) {
+	$(".sub-slide .wrap").stop().animate({
+		"left": -100 * subNow + "%"
+	}, 500, function () {
+		if (subNow == subLast) {
 			subNow = 0;
 			$(".sub-slide .wrap").css("left", 0);
 		}
 	});
 }
 
-//.navi에서의 서브 카테고리 생성
-function columnMaker (data) {
+// .navi에서의 서브카테고리 생성
+function columnMaker(data) {
 	var html = '';
-	html += '		<div class="subs">';
-	for(var i in data) {
-		html += '			<div class="sub">';
-		html += '				<div class="title">'+data[i].title+'</div>';
-		for(var j in data[i].subs) {
-			html += '			<div class="name rel">'+data[i].subs[j].title;
-			if(data[i].subs[j].icon != ''){
-				html += '<div class="icon '+data[i].subs[j].color+'">'+data[i].subs[j].icon;
+	html += '<div class="subs">';
+	for (var i in data) {
+		html += '<div class="sub">';
+		html += '<div class="title">' + data[i].title + '</div>';
+		for (var j in data[i].subs) {
+			html += '<div class="name rel">' + data[i].subs[j].title;
+			if (data[i].subs[j].icon != '') {
+				html += '<div class="icon ' + data[i].subs[j].color + '">' + data[i].subs[j].icon;
 				html += '<i class="fas fa-caret-right"></i>';
 				html += '</div>';
 			}
-			html +='</div>';
+			html += '</div>';
 		}
 		html += '</div>';
 	}
 	html += '</div>';
-	return (html);
+	return html;
 }
 
+/** 이벤트 콜백 *************************************/
 
-/***** 이벤트 콜백 *************************/
-
-//	.navi 의 sub-wrap 토글
+// .navi 의 .sub-wrap 토글
 function onEnter() {
 	$(this).find(".sub-wrap").css("display", "flex");
 }
 
-//	.navi 의 sub-wrap 토글
+// .navi 의 .sub-wrap 토글
 function onLeave() {
 	$(this).find(".sub-wrap").css("display", "none");
 }
 
-//	.navi.FULL의 슬라이드 이미지 교체 
+// .navi.FULL 의 슬라이드 이미지 교체
 function onColorClick() {
 	$(this).addClass("active").siblings().removeClass("active");
 	var $imgCase = $(this).parent().prev().find(".img-case");
@@ -59,41 +59,40 @@ function onColorClick() {
 	$imgCase.eq($(this).index()).stop().delay(100).fadeIn(100);
 }
 
-//	.navi.FULL의 슬라이드 PREV
+// .navi.FULL 의 슬라이드 PREV
 function onSubPrevClick() {
-	if(subNow == 0) {
+	if (subNow == 0) {
 		subNow = subLast - 1;
-		$(".sub-slide .wrap").css("left", -subLast * 100 +"%");
-	}
-	else subNow--;
+		$(".sub-slide .wrap").css("left", -subLast * 100 + "%");
+	} else subNow--;
 	subAni();
 }
 
-//	.navi.FULL의 슬라이드 NEXT
+// .navi.FULL 의 슬라이드 NEXT
 function onSubNextClick() {
-	if(subNow < subLast) subNow++;
+	if (subNow < subLast) subNow++;
 	subAni();
 }
 
-//	.navi의 ajax 콜백
+// .navi의 ajax 콜백
 function onNaviLoad(r) {
-	//console.log(r.navs);
+	// console.log(r.navs);
 	var html = '';
-	for(var i in r.navs) {
-		html  = '<div class="navi '+r.navs[i].class+'">';
-		html += '<div class="title">'+r.navs[i].title+' <i class="fa fa-angle-down"></i>';
-		if(r.navs[i].icon != '') {
-			html += '<div class="icon '+r.navs[i].color+'">'+r.navs[i].icon;
+	for (var i in r.navs) {
+		html = '<div class="navi ' + r.navs[i].class + '">';
+		html += '<div class="title">' + r.navs[i].title + ' <i class="fa fa-angle-down"></i>';
+		if (r.navs[i].icon != '') {
+			html += '<div class="icon ' + r.navs[i].color + '">' + r.navs[i].icon;
 			html += '<i class="fas fa-caret-right"></i>';
 			html += '</div>';
-		} 
+		}
 		html += '</div>';
 		html += '<div class="sub-wrap">';
-		if(r.navs[i].class.indexOf('IMAGE') > -1) {
-			for(var j in r.navs[i].subs) {
+		if (r.navs[i].class.indexOf('IMAGE') > -1) {
+			for (var j in r.navs[i].subs) {
 				html += '<div class="sub">';
-				html += '<div class="title">'+r.navs[i].subs[j].title+'</div>';
-				html += '<div class="cont-img"><img src="'+r.navs[i].subs[j].src+'" class="w-100"></div>';
+				html += '<div class="title">' + r.navs[i].subs[j].title + '</div>';
+				html += '<div class="cont-img"><img src="' + r.navs[i].subs[j].src + '" class="w-100"></div>';
 				html += '</div>';
 			}
 		}
@@ -101,7 +100,7 @@ function onNaviLoad(r) {
 			html += '<div class="wrapper">';
 			html += '	<div class="lt">';
 			html += columnMaker(r.navs[i].subs);
-			//console.log(html);
+			// console.log(html);
 			html += '		<div class="infos">';
 			for (var j in r.navs[i].infos) {
 				html += '<div class="info">';
@@ -181,22 +180,63 @@ function onNaviLoad(r) {
 		}
 		html += '</div>'; // .sub-wrap
 		html += '</div>'; // .navi
-		//console.log(html);
+		// console.log(html);
 		$(".navi-wrap").append(html);
+	}
+	// .mo-navi 생성
+	for(var i in r.navs) {
+		html  = '<li class="mo-navi">';
+		html += '<div class="title">'+r.navs[i].title+'</div>';
+		html += '<div class="bt-down">';
+		html += '	<div class="slash slash-lt"></div>';
+		html += '	<div class="slash slash-rt"></div>';
+		html += '</div>';
+		html += '<ul class="mo-sub">';
+		html += '	<i class="mo-caret fa fa-caret-up"></i>';
+		if(r.navs[i].subs && r.navs[i].subs.length == 1) {
+			r.navs[i].subs = r.navs[i].subs[0].subs;
+		}
+		for(var j in r.navs[i].subs) {
+			html += '<li class="mo-sub-navi">';
+			html += '<div class="title">'+r.navs[i].subs[j].title+'</div>';
+			// console.log(r.navs[2].subs[0].subs);
+			if(r.navs[i].subs[j].subs && r.navs[i].subs[j].subs.length > 0) {
+				html += '<div class="bt-down">';
+				html += '	<div class="slash slash-lt"></div>';
+				html += '	<div class="slash slash-rt"></div>';
+				html += '</div>';
+				html += '<ul class="mo-sub-sub">';
+				html += '	<i class="mo-caret fa fa-caret-up"></i>';
+				for(var k in r.navs[i].subs[j].subs) {
+					html += '<li>'+r.navs[i].subs[j].subs[k].title+'</li>';
+				}
+				html += '</ul>';
+			}
+			html += '</li>';
+		}
+		html += '</ul>';
+		html += '</li>';
+		$(".mo-navi-wrap").append(html);
 	}
 	$(".navi-wrap > .navi").mouseenter(onEnter);
 	$(".navi-wrap > .navi").mouseleave(onLeave);
 	$(".sub-slide .color").find("span").click(onColorClick);
 	$(".sub-slide .wrap").swipe({
-		swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-			if(direction == 'left') $(".sub-slide .bt-next").trigger("click");
-			if(direction == 'right') $(".sub-slide .bt-prev").trigger("click");
+		swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+			if (direction == 'left') $(".sub-slide .bt-next").trigger("click");
+			if (direction == 'right') $(".sub-slide .bt-prev").trigger("click");
 		},
 		threshold: 30
 	});
 	$(".sub-slide .bt-prev").on("click", onSubPrevClick);
 	$(".sub-slide .bt-next").on("click", onSubNextClick);
+	$(".navi-mo-icon").on("click", onNaviMoClick); // .navi-mo-icon 클릭 
+	$(".mo-wrapper").on("click", onMoWrapperClick);	// .mo-wrapper 클릭
+	$(".mo-wrap").on("click", onMoWrapClick);	// .mo-wrap 클릭
+	$(".mo-navi .bt-down").on("click", onMoNaviClick);	// .mo-navi .bt-down 클릭
 }
+
+
 
 // .navi-mo-icon click 콜백
 function onNaviMoClick(e) {
@@ -213,78 +253,72 @@ function onMoWrapperClick(e) {
 	$(".mo-wrap").css("left", "-270px");
 }
 
-//	.mo-wrap click 콜백
+// .mo-wrap click 콜백
 function onMoWrapClick(e) {
 	e.stopPropagation();
 }
 
-//	.mo-navi click 콜백
+// .mo-navi click 콜백
 function onMoNaviClick(e) {
 	$(this).toggleClass("active");
+	if($(this).hasClass("active")) $(this).next().stop().slideDown(300);
+	else $(this).next().stop().slideUp(300);
 }
 
-//	resize 콜백
+
+// resize 콜백
 function onResize(e) {
-
+	var winWid = $(this).outerWidth();
+	if(winWid > 991 && $(".mo-wrapper").css("display") == 'block') {
+		$(".mo-wrapper").trigger("click");
+	}
 }
 
-//	window scroll 콜백
+// window scroll 콜백
 function onScroll(e) {
 	var scTop = $(this).scrollTop();
-
-	//	header의 fixed
+	
+	// header의 fixed
 	if(scTop > 180) {
-		$(".top-wrapper").css("display","none");
-		$(".search-wrapper").css("display","none");
-		$(".header-wrapper").css({
-			"position":"fixed",
-			"top":0,
-			"box-shadow":"0 0 8px rgba(0,0,0,0.3)",
-	});
+		$(".top-wrapper").css("display", "none");
+		$(".search-wrapper").css("display", "none");
+		$(".header-wrapper").css({ "position": "fixed", "top": 0, 
+		"box-shadow": "0 0 6px rgba(0,0,0,0.3)" });
 	}
 	else {
-		$(".top-wrapper").css("display","block");
-		$(".search-wrapper").css("display","flex");
-		$(".header-wrapper").css({
-			"position":"static",
-			"top":"-85px",
-			"box-shadow":"none"
-	});
+		$(".top-wrapper").css("display", "block");
+		$(".search-wrapper").css("display", "flex");
+		$(".header-wrapper").css({ "position": "static", "top": "-85px", "box-shadow": "none" });
 	}
 }
 
-//	.mo-wrapper 콜백
+// .mo-wrapper scroll 콜백
 function onMobileScroll(e) {
 	e.stopPropagation();
 	e.preventDefault();
-	/* $("html,body").css({
-		"overflow":"hidden",
-		"height":"100vh"
-	}); */
+}
+
+function onMobileWrapScroll(e) {
+	e.stopPropagation();
+	var winHei = $(window).outerHeight();
+	var meHei = $(this).find(".mo-navi-wrap").outerHeight();
+	if(meHei <= winHei) e.preventDefault();
 }
 
 
-/***** 이벤트 등록 *************************/
+/** 이벤트 등록 **********************/
 
-//	Main Navi 생성
+
+// Main Navi 생성
 $.get('../json/navi.json', onNaviLoad);
 
-//	.navi-mo-icon 클릭 
-$(".navi-mo-icon").on("click",onNaviMoClick);
-
-//	.mo-wrapper 클릭 
-$(".mo-wrapper").on("click",onMoWrapperClick);
-
-//	.mo-wrap 클릭 
-$(".mo-wrap").on("click",onMoWrapClick);
-
-//	.mo-navi .slash 클릭 
-$(".mo-navi .bt-down").on("click",onMoNaviClick);
-
 // 스크롤 이벤트
-$(window).on("scroll",onScroll);
-$(".mo-wrapper").on("scroll touchmove mousewheel",onMobileScroll);
+$(window).on("scroll", onScroll);
+$(".mo-wrapper").on("scroll touchmove mousewheel", onMobileScroll);
+$(".mo-wrap").on("scroll touchmove mousewheel", onMobileWrapScroll);
 
 // 리사이즈 이벤트
-$(window).on("resize",onResize);
+$(window).on("resize", onResize);
+
+
 
